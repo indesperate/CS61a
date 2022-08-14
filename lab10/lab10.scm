ce7144cc51f7a37fde95b00788b5bb8b059fe4ca
indesperate <indesperate@outlook.com>
2022-08-14 18:19:26 +0800

    finish lab10

---
+++ lab10/lab10.scm
@@ -0,0 +1,28 @@
+(define (over-or-under num1 num2) 
+    (cond
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1)
+        ))
+
+(define (make-adder num) 
+    (define (make inc) (+ num inc))
+    make
+)
+
+(define (composed f g) 
+    (lambda (x)(f (g x)))
+)
+
+(define lst 
+    (cons (cons 1 nil) (cons 2 (cons (cons 3 (cons 4 nil)) (cons 5 nil))))
+)
+
+(define (remove item lst) 
+    (if (null? lst)  nil
+    (
+        if (= (car lst) item) (remove item (cdr lst))
+        (cons (car lst) (remove item (cdr lst)))
+    )
+    )
+)
